01a19ae770aa76b5c03868196fbbd11be37ea86d
Munira-K <kurbanovamunira2003@gmail.com>
2025-03-07 17:48:11 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-import streamlit as st
-import pandas as pd
+# import streamlit as st
+# import pandas as pd
 
-pages = [
-    st.Page('about.py', title = 'О проекте 🫡'), 
-    st.Page('eda.py', title = 'Обзор 🔍'),
-    st.Page('ml.py', title = 'Модели 💃'),
-    st.Page('feature_imp.py', title = 'Важность признаков 🔗'),
-    st.Page('metrics2.py', title = 'Результаты ⚖️')
-]
-pg_h = st.navigation(pages)
-# pg_h.run()
+# pages = [
+#     st.Page('about.py', title = 'О проекте 🫡'), 
+#     st.Page('eda.py', title = 'Обзор 🔍'),
+#     st.Page('ml.py', title = 'Модели 💃'),
+#     st.Page('feature_imp.py', title = 'Важность признаков 🔗'),
+#     st.Page('metrics2.py', title = 'Результаты ⚖️')
+# ]
+# pg_h = st.navigation(pages)
+# # pg_h.run()
