d1359f8a15f94bd6216df935d2b9893517709def
Munira-K <kurbanovamunira2003@gmail.com>
2025-03-07 14:57:38 +0500

    Update data.py

--- streamlit/data.py
+++ streamlit/data.py
@@ -5,8 +5,8 @@ import warnings
 
 
 def load_data():
-    test = pd.read_csv('proj1\source\test.csv')
-    train = pd.read_csv('proj1/source/train.csv')
+    test = pd.read_csv('C:\Users\user\Desktop\ds_course\proj1\source\test.csv', sep=",")
+    train = pd.read_csv('C:\Users\user\Desktop\ds_course\proj1\source\train.csv', sep=",")
     data = pd.concat([test, train])
     data = data.sample(129880).reset_index().drop(['index', 'id'], axis=1)
     data = data.drop(['Unnamed: 0', 'Arrival Delay in Minutes', 'Departure Delay in Minutes'], axis=1)
